1d825b82a7895d371e42160fba5b03d9beddd670
kthurm <karalthurmond@gmail.com>
2017-05-22 16:59:29 -0400

    mobile fixes
    fixed mobile collapse

--- js/main.js
+++ js/main.js
@@ -20,42 +20,6 @@ $(document).ready(function() {
 		}
 	});
 
-	$('#about .blue-circle').waypoint(function() {
-		$('#about .blue-circle').addClass('animated fadeInUp')
-	}, {
-		offset: '50%'
-	});
-
-	$('#features .blue-circle').waypoint(function() {
-		$(this.element).addClass('animated fadeInUp')
-	}, {
-		offset: '50%'
-	});
-
-	$('.features-image img').waypoint(function() {
-		$('.features-image img').addClass('animated rubberBand')
-	}, {
-		offset: '50%'
-	});
-
-	$('#screenshots .col-xs-4').waypoint(function() {
-		$(this.element).addClass('animated zoomIn');
-		$(this.element).css({'opacity':1});
-	}, {
-		offset: '50%'
-	});
-
-	$('#download div.phone img').waypoint(function() {
-		$(this.element).addClass('animated fadeInRight');
-	}, {
-		offset: '50%'
-	});
-
-	$('#download .platforms  > div').waypoint(function() {
-		$(this.element).addClass('animated fadeInUp');
-	}, {
-		offset: '50%'
-	});
 
 	
 });
@@ -64,5 +28,16 @@ smoothScroll.init({
 	speed:700,
 	easing: 'easeInOutQuad',
 	updateURL: false,
-	offset:60
+	offset:70
+});
+
+$(document).ready(function() { 
+
+$("body").click(function(event) {
+        // only do this if navigation is visible, otherwise you see jump in navigation while collapse() is called 
+         if ($(".navbar-collapse").is(":visible") && $(".navbar-toggle").is(":visible") ) {
+            $('.navbar-collapse').collapse('toggle');
+        }
+  });
+
 });
